a52063048b71d74b288e26a239fa8d30a4ad1de6
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-02-16 09:01:56 -0500

    Create .rb

---
+++ .rb
@@ -0,0 +1,11 @@
+puts "How many hours are in a year?"
+puts 24 * 365 
+
+puts "How many minutes are in a decade?"
+puts 60 * 24 * 365 * 10
+
+puts "How many seconds old am I?"
+puts 60 * 60 * 24 * 365 * 29
+
+puts "If someone is 1,160,000,000 seconds old, then what is their age in years?"
+puts 1160000000 / (60 * 60 * 24 * 365)
